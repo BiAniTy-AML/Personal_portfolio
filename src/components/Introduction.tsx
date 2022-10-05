c83d897a4d5c6ca5f03a4a574d34bf77400cc902
BiAniTy <drayttc@gmail.com>
2022-10-05 11:36:56 -0300

    Improves projects section design

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -8,21 +8,22 @@ const Intro: FC<Props> = () => {
     return (
         <section className="intro_container section_cont">
             {/* Photo by <a href="https://unsplash.com/@degleex?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Degleex</a> on <a href="https://unsplash.com/s/photos/background-forest?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> */}
-
             <img src={prof_pic} alt="" className="profile_pic" />
 
             <div className="greetings">
                 <h2 className="header">About me</h2>
 
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Labore qui in aperiam voluptatibus, nobis quo voluptatem
-                    veritatis esse. Repudiandae, esse! Iusto laboriosam optio
-                    recusandae, soluta porro assumenda voluptatibus animi, in
-                    veritatis voluptatum minima ipsa natus ducimus neque
-                    doloremque saepe, placeat fuga dolor eaque? Aliquid commodi
-                    quisquam ea dolorum, iusto voluptatem.
-                </p>
+                <div className="details">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Labore qui in aperiam voluptatibus, nobis quo voluptatem
+                        veritatis esse. Repudiandae, esse! Iusto laboriosam
+                        optio recusandae, soluta porro assumenda voluptatibus
+                        animi, in veritatis voluptatum minima ipsa natus ducimus
+                        neque doloremque saepe, placeat fuga dolor eaque?
+                        Aliquid commodi quisquam ea dolorum, iusto voluptatem.
+                    </p>
+                </div>
             </div>
         </section>
     );
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,22 +10,98 @@ const Projects: FC<Props> = () => {
             <div className="projects">
                 <div className="project">
                     <img src="" alt="" className="screenshot" />
-                    <div className="description">Project 1</div>
+                    <div className="details">
+                        <div className="main_info">
+                            <div className="name">Project 01</div>
+                            <div className="icons">
+                                <a href="#">
+                                    <img
+                                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                                        alt=""
+                                        className="icon"
+                                    />
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="description">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Dolores reprehenderit illo perferendis ducimus
+                            vel excepturi fugit totam asperiores maxime dolorum?
+                        </div>
+                    </div>
                 </div>
 
                 <div className="project">
                     <img src="" alt="" className="screenshot" />
-                    <div className="description">Project 2</div>
+                    <div className="details">
+                        <div className="main_info">
+                            <div className="name">Project 02</div>
+                            <div className="icons">
+                                <a href="#">
+                                    <img
+                                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                                        alt=""
+                                        className="icon"
+                                    />
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="description">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Hic, nam? Esse alias, sed molestias dolore
+                            ipsa recusandae dicta nobis eaque.
+                        </div>
+                    </div>
                 </div>
 
                 <div className="project">
                     <img src="" alt="" className="screenshot" />
-                    <div className="description">Project 3</div>
-                </div>
+                    <div className="details">
+                        <div className="main_info">
+                            <div className="name">Project 03</div>
+                            <div className="icons">
+                                <a href="#">
+                                    <img
+                                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                                        alt=""
+                                        className="icon"
+                                    />
+                                </a>
+                            </div>
+                        </div>
 
+                        <div className="description">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing
+                            elit. Ipsum blanditiis modi autem cum totam eos
+                            soluta reprehenderit, placeat magni assumenda?
+                        </div>
+                    </div>
+                </div>
                 <div className="project">
                     <img src="" alt="" className="screenshot" />
-                    <div className="description">Project 4</div>
+                    <div className="details">
+                        <div className="main_info">
+                            <div className="name">Project 04</div>
+                            <div className="icons">
+                                <a href="#">
+                                    <img
+                                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg"
+                                        alt=""
+                                        className="icon"
+                                    />
+                                </a>
+                            </div>
+                        </div>
+
+                        <div className="description">
+                            Lorem ipsum dolor sit amet consectetur, adipisicing
+                            elit. Dolorum, amet quibusdam. Dignissimos dolore
+                            voluptates reiciendis exercitationem quibusdam unde
+                            delectus quasi.
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
