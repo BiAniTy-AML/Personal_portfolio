505c23b1ee73f0be9540927111345c79b55220a0
BiAniTy <drayttc@gmail.com>
2023-08-08 18:24:49 -0300

    Improves typing animation - works on reverse

--- src/components/AnimatedWords.tsx
+++ src/components/AnimatedWords.tsx
@@ -4,6 +4,8 @@ interface Props {
     text_before: string;
     text_typed: string;
     delay: number;
+    delay_reverse: number;
+    delay_between?: number;
     infinite: boolean;
 }
 
@@ -11,42 +13,82 @@ const AnimatedWords: FC<Props> = ({
     text_before,
     text_typed,
     delay,
+    delay_reverse,
+    delay_between = 1000,
     infinite,
 }) => {
     const div_ref = useRef<HTMLDivElement | null>(null);
 
-    const [currentText, setCurrentText] = useState("");
-    const [currentIndex, setCurrentIndex] = useState(0);
+    const [current_text, set_current_text] = useState("");
+    const [current_index, set_current_index] = useState(0);
+    const [reverse_index, set_reverse_index] = useState(0);
+
+    const clear_timeouts = (timeouts: NodeJS.Timeout[]) => {
+        timeouts.forEach((timeout) => {
+            clearTimeout(timeout);
+        });
+    };
 
     useEffect(() => {
         let timeout: NodeJS.Timeout;
 
-        if (currentIndex <= text_typed.length) {
+        if (current_index <= text_typed.length) {
             timeout = setTimeout(() => {
-                setCurrentText(
-                    (prevText) => prevText + text_typed[currentIndex]
+                set_current_text(
+                    (prevText) => prevText + text_typed[current_index]
                 );
-                setCurrentIndex((prevIndex) => prevIndex + 1);
+                set_current_index((prevIndex) => prevIndex + 1);
             }, delay);
-        } else if (infinite) {
-            setCurrentIndex(0);
-            setCurrentText("");
+
+            if (current_index === text_typed.length) {
+                clearTimeout(timeout);
+
+                set_current_index((prevIndex) => prevIndex + 1);
+
+                setTimeout(() => {
+                    set_reverse_index(text_typed.length);
+                }, delay_between);
+            }
+        } else if (infinite && reverse_index > 0) {
+            timeout = setTimeout(() => {
+                set_current_text((prevText) => prevText.slice(0, -1));
+                set_reverse_index((prev_index) => prev_index - 1);
+            }, delay_reverse);
+
+            if (reverse_index === 1) {
+                setTimeout(() => {
+                    set_current_index(0);
+                    set_current_text("");
+                    set_reverse_index(0);
+                    clearTimeout(timeout);
+                }, delay_between);
+            }
         }
 
-        return () => clearTimeout(timeout);
-    }, [currentIndex, delay, infinite, text_typed]);
+        return () => {
+            clearTimeout(timeout);
+        };
+    }, [
+        current_index,
+        delay,
+        delay_reverse,
+        delay_between,
+        infinite,
+        text_typed,
+        reverse_index,
+    ]);
 
     return (
         <p
             ref={div_ref}
             // onLoad={() => wordflick()}
-            className="text-lg text-white w-fit mx-auto relative 
-            \ after:content-['\_'] after:absolute after:-right-3 after:bottom-[2px] after:max-h-full 
-            \ after:font-bold after:animate-pulse-fast
-            "
+            className="text-2xl text-white w-fit mx-auto relative min-h-[4em]"
         >
             {text_before}
-            {currentText}
+            {current_text}
+            <span className="font-bold animate-pulse-fast text-2xl inline-block -translate-y-[6px]">
+                _
+            </span>
         </p>
     );
 };
--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -7,20 +7,26 @@ interface Props {}
 
 const Intro: FC<Props> = () => {
     return (
-        <section className="bg-gray-950 text-white" id="intro">
+        <section
+            className="bg-gray-950 text-white flex flex-col justify-center px-2"
+            id="intro"
+        >
             {/* Photo by <a href="https://unsplash.com/@degleex?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Degleex</a> on <a href="https://unsplash.com/s/photos/background-forest?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> */}
 
+            {/*
             <img src={prof_pic} alt="" className="w-1/2 h-auto mx-auto" />
+            */}
 
-            <div className="max-w-[80vw] mx-auto py-[40px]">
+            <AnimatedWords
+                text_before={"I am a "}
+                text_typed={"Fullstack web developer"}
+                delay={150}
+                delay_reverse={200}
+                infinite={true}
+            />
+
+            <div className="max-w-[80vw] mx-auto py-[40px] show-up px-4">
                 <h2 className="text-center text-lg font-bold">About me</h2>
-
-                <AnimatedWords
-                    text_before={"I am a "}
-                    text_typed={"Fullstack web developer"}
-                    delay={150}
-                    infinite={true}
-                />
 
                 <div className="details">
                     <p>
